3e77503cbead4df8c6d2246d4d1f35f1f254b3ec
MatheusR42 <matheus.araujo.r42@gmail.com>
2021-05-07 13:33:37 -0300

    get data from Product Field

--- react/ProductCustom.tsx
+++ react/ProductCustom.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import { useProduct } from 'vtex.product-context'
+import type { Product } from 'vtex.product-context/react/ProductTypes'
+
+const getProductField = (product: Product, fieldName: string) => {
+  return (
+    product.properties.find((item) => item.name === fieldName)?.values?.[0] ??
+    ''
+  )
+}
 
 const ProductCustom = () => {
-  const { selectedItem } = useProduct() ?? {}
+  const { selectedItem, product } = useProduct() ?? {}
+  let type = ''
 
-  return <h1>selected item: {selectedItem?.name}</h1>
+  if (product) {
+    type = getProductField(product, 'Tipo')
+  }
+
+  return (
+    <>
+      <h1>selected item: {selectedItem?.name}</h1>
+      <h2>Tipo: {type}</h2>
+    </>
+  )
 }
 
 export default ProductCustom
